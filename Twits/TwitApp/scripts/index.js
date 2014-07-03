365a7f9fdd27cdf985e7ffa07dddabd1c4b1b2ef
recreationalcalculus <recreationalcalculus@gmail.com>
2014-07-03 00:09:59 -0500

    Register/Login/Tweet HTML built, angular controller wired up to index.html but not implemented.

--- Twits/TwitApp/scripts/index.js
+++ Twits/TwitApp/scripts/index.js
@@ -1,1 +1,10 @@
-﻿
+﻿var Twit = angular.module('Twit', []);
+
+Twit.controller('LoginTweet', function ($scope) {
+    $scope.loggedIn = false;
+    $scope.user = 'Anonymous';
+
+    $scope.Login = function () {
+        $scope.loggedIn = true;
+    }
+});
